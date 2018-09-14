623e42d66cc1a2458c325158c29d9da1f198adfb
Brad Douglas <bdouglas@bozc02txfm7htd6.bozelljacobs.com>
2018-09-14 09:14:10 -0500

    Updates

--- js/site/speakers.js
+++ js/site/speakers.js
@@ -18,10 +18,7 @@ speakers.forEach(speaker => {
         const headlines = document.querySelectorAll('.speakers__heading');
         const notActive = document.querySelectorAll('.speaker:not(.active)');
         const hiddenBio = speaker.querySelector('.speaker__bio');
-        const bioContainer = document.querySelector('.speaker-bio__container');
-        let theBioInner = document.querySelector('.speaker-bio__inner');
-        theBioInner.innerHTML = hiddenBio.innerHTML;
-        const bioPs = theBioInner.querySelectorAll('p');
+        const pattern = document.querySelector('.speaker-bio__pattern');
 
         const allTheThings = document.querySelectorAll(
             '.speaker:not(.active), .speakers__heading, .interior-hero '
@@ -29,40 +26,85 @@ speakers.forEach(speaker => {
 
         const tl = new TimelineMax();
 
-        tl.set(bioPs, { autoAlpha: 0 })
-            .set(bioContainer, { autoAlpha: 1, zIndex: 5 })
-            .set(root, { className: '+=no-scroll' })
-            .set(body, { className: '+=no-scroll' })
-            .set(footer, { autoAlpha: 0, ease: Expo.easeInOut })
-            .to(
-                allTheThings,
-                0.9,
-                { scale: 1.3, autoAlpha: 0, ease: Expo.easeInOut },
-                0.1
-            )
-            .call(flip, [speaker], this, '-=0.7')
-            .staggerFromTo(
-                bioPs,
-                1,
-                { y: 40 },
-                { y: 0, autoAlpha: 1, ease: Expo.easeOut },
-                0.15
+        if (window.innerWidth < 750) {
+            const bioPs = hiddenBio.querySelectorAll('p');
+            tl.set(bioPs, { autoAlpha: 0 })
+                .set(hiddenBio, {
+                    display: 'block',
+                    position: 'relative',
+                    zIndex: 12,
+                    autoAlpha: 1,
+                })
+                .set(root, { className: '+=no-scroll' })
+                .set(body, { className: '+=no-scroll' })
+                .set(footer, { autoAlpha: 0, ease: Expo.easeInOut })
+                .to(
+                    allTheThings,
+                    0.9,
+                    { scale: 1.3, autoAlpha: 0, ease: Expo.easeInOut },
+                    0.1
+                )
+                .call(flip, [speaker], this, '-=0.7')
+                .staggerFromTo(
+                    bioPs,
+                    1,
+                    { y: 40 },
+                    { y: 0, autoAlpha: 1, ease: Expo.easeOut },
+                    0.15
+                );
+        } else {
+            const bioContainer = document.querySelector(
+                '.speaker-bio__container'
             );
+            let theBioInner = document.querySelector('.speaker-bio__inner');
+            theBioInner.innerHTML = hiddenBio.innerHTML;
+            const bioPs = theBioInner.querySelectorAll('p');
+
+            tl.set(bioPs, { autoAlpha: 0 })
+                .set(bioContainer, { autoAlpha: 1, zIndex: 5 })
+                .set(root, { className: '+=no-scroll' })
+                .set(body, { className: '+=no-scroll' })
+                .set(footer, { autoAlpha: 0, ease: Expo.easeInOut })
+                .to(
+                    allTheThings,
+                    0.9,
+                    { scale: 1.3, autoAlpha: 0, ease: Expo.easeInOut },
+                    0.1
+                )
+                .call(flip, [speaker], this, '-=0.7')
+                .staggerFromTo(
+                    bioPs,
+                    1,
+                    { y: 40 },
+                    { y: 0, autoAlpha: 1, ease: Expo.easeOut },
+                    0.15
+                )
+                .to(
+                    pattern,
+                    0.8,
+                    { autoAlpha: 1, ease: Expo.easeOut },
+                    '-=0.4'
+                );
+        }
     });
 });
 
 function flip(speaker) {
     const inner = speaker.querySelector('.speaker__inner');
-    const margin =
-        (window.innerHeight - speaker.getBoundingClientRect().height) / 2 / 2 +
-        'px';
-
     const first = inner.getBoundingClientRect();
 
     speaker.classList.add('expanding');
 
-    inner.style.top = margin;
-    inner.style.marginTop = margin;
+    if (window.innerWidth > 750) {
+        const margin =
+            (window.innerHeight - speaker.getBoundingClientRect().height) /
+                2 /
+                2 +
+            'px';
+        inner.style.marginTop = margin;
+        inner.style.top = margin;
+    }
+
     inner.classList.add('move');
 
     const last = inner.getBoundingClientRect();
