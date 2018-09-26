0ab7e3098b69f21e8443d25635e779c8a0f2c0f2
Brad Douglas <bdouglas@bozc02txfm7htd6.bozelljacobs.com>
2018-09-26 16:35:00 -0500

    agenda changes

--- js/site/test.js
+++ js/site/test.js
@@ -1,17 +1,53 @@
 const eventTitles = document.querySelectorAll('.event__title');
+const eventCloseBtns = document.querySelectorAll('.mobile-desc__close');
+const body = document.body;
+const docEl = document.documentElement;
+//Disable/Enable Scroll Things
+// Scroll control functions. Taken from https://stackoverflow.com/a/4770179.
+const keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+const preventDefault = e => {
+    e = e || window.event;
+    if (e.preventDefault) e.preventDefault();
+    e.returnValue = false;
+};
+const preventDefaultForScrollKeys = e => {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+};
+const disableScroll = () => {
+    if (window.addEventListener)
+        // older FF
+        window.addEventListener('DOMMouseScroll', preventDefault, false);
+    window.onwheel = preventDefault; // modern standard
+    window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+    window.ontouchmove = preventDefault; // mobile
+    document.onkeydown = preventDefaultForScrollKeys;
+};
+const enableScroll = () => {
+    if (window.removeEventListener)
+        window.removeEventListener('DOMMouseScroll', preventDefault, false);
+    window.onmousewheel = document.onmousewheel = null;
+    window.onwheel = null;
+    window.ontouchmove = null;
+    document.onkeydown = null;
+};
 
 eventTitles.forEach(title => {
     const description = title.nextElementSibling;
-    const arrow = title.querySelector('.arrow-forward');
     title.addEventListener('click', e => {
         e.stopPropagation;
-        // description.style.maxHeight = description.scrollHeight + 'px';
-        if (!title.classList.contains('open')) {
-            title.classList.add('open');
-            description.style.transform = 'none';
-        } else {
-            title.classList.remove('open');
-            description.style.transform = 'translateX(100%)';
-        }
+        description.style.transform = 'none';
+        disableScroll();
+    });
+});
+
+eventCloseBtns.forEach(btn => {
+    const container = btn.parentElement.parentElement;
+    btn.addEventListener('click', e => {
+        e.stopPropagation;
+        container.style.transform = 'translateX(100%)';
+        enableScroll();
     });
 });
