3d81fb35ffb03d9c47e09bafe6bacb5e0b581145
Brad Douglas <bdouglas@bozc02txfm7htd6.bozelljacobs.com>
2018-09-26 16:54:08 -0500

    Minified JS file

--- js/site/site.js
+++ js/site/site.js
@@ -632,6 +632,62 @@ const filters = () => {
     });
 };
 
+const mobileAgendaEventFunctionality = () => {
+    const eventTitles = document.querySelectorAll('.event__title');
+    const eventCloseBtns = document.querySelectorAll('.mobile-desc__close');
+    const body = document.body;
+    const docEl = document.documentElement;
+    //Disable/Enable Scroll Things
+    // Scroll control functions. Taken from https://stackoverflow.com/a/4770179.
+    const keys = { 37: 1, 38: 1, 39: 1, 40: 1 };
+    const preventDefault = e => {
+        e = e || window.event;
+        if (e.preventDefault) e.preventDefault();
+        e.returnValue = false;
+    };
+    const preventDefaultForScrollKeys = e => {
+        if (keys[e.keyCode]) {
+            preventDefault(e);
+            return false;
+        }
+    };
+    const disableScroll = () => {
+        if (window.addEventListener)
+            // older FF
+            window.addEventListener('DOMMouseScroll', preventDefault, false);
+        window.onwheel = preventDefault; // modern standard
+        window.onmousewheel = document.onmousewheel = preventDefault; // older browsers, IE
+        window.ontouchmove = preventDefault; // mobile
+        document.onkeydown = preventDefaultForScrollKeys;
+    };
+    const enableScroll = () => {
+        if (window.removeEventListener)
+            window.removeEventListener('DOMMouseScroll', preventDefault, false);
+        window.onmousewheel = document.onmousewheel = null;
+        window.onwheel = null;
+        window.ontouchmove = null;
+        document.onkeydown = null;
+    };
+
+    eventTitles.forEach(title => {
+        const description = title.nextElementSibling;
+        title.addEventListener('click', e => {
+            e.stopPropagation;
+            description.style.transform = 'none';
+            disableScroll();
+        });
+    });
+
+    eventCloseBtns.forEach(btn => {
+        const container = btn.parentElement.parentElement;
+        btn.addEventListener('click', e => {
+            e.stopPropagation;
+            container.style.transform = 'translateX(100%)';
+            enableScroll();
+        });
+    });
+};
+
 /*
 =====================================
 Speaker Functions
@@ -1262,17 +1318,12 @@ Agenda View
 */
 const Agendapage = Barba.BaseView.extend({
     namespace: 'agenda',
-    onEnter() {
-        // fixedAgenda();
-        // scrollAgenda();
-        // scrollSideNav();
-        // showFilters();
-        // filters();
-    },
+    onEnter() {},
     onEnterCompleted() {
         // The Transition has just finished.
         fixedAgenda();
         scrollAgenda();
+        mobileAgendaEventFunctionality();
         // scrollSideNav();
     },
     onLeave() {
